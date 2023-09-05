681f111f53f099c6bce4ce6cd4d5f8890b07f6c9
Ilan Laloum <isramail@gmail.com>
2023-09-05 14:22:14 +0300

    Add error handliing

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify,render_template
 from googlesearch import search
 
 app = Flask(__name__)
+ERROR_MESSAGE:str = "אירעה שגיאה"
 
 @app.route("/")
 def form():
@@ -17,38 +18,47 @@ def chat():
         bot_response = get_answer(question=question,n=3,cutoff=0.5)
 
         return jsonify({'response': bot_response})
-    except:
-        return jsonify({'response': None })
+    except Exception as e:
+        return jsonify({'response': {'message':ERROR_MESSAGE,'data':repr(e)} })
 
 @app.route('/api/suggestions', methods=['POST'])
 def suggestions():
-    data = request.get_json()
-    question = data['message']
-    bot_response = get_suggestions(question)
-    return jsonify({'suggestions': bot_response})
+    try:
+        data = request.get_json()
+        question = data['message']
+        bot_response = get_suggestions(question)
+        return jsonify({'suggestions': bot_response})
+    except Exception as e:
+        return jsonify({'response': {'error':ERROR_MESSAGE,'data':repr(e)} })
 
 @app.route('/api/links', methods=['POST'])
 def links():
-    data = request.get_json()
-    question = data['message']
-    bot_response = get_links(question)
-    return jsonify({'links': bot_response})
+    try:
+        data = request.get_json()
+        question = data['message']
+        bot_response = get_links(question)
+        return jsonify({'links': bot_response})
+    except Exception as e:
+        return jsonify({'response': {'error':ERROR_MESSAGE,'data':repr(e)} })
 
 @app.route('/api/search', methods=['POST'])
 def search_with_google():
-    data = request.get_json()
-    question = data['message']
-    limit = data['limit'] or 1 # this will result in 1
-    if limit>10: 
-        limit=10
+    try:
+        data = request.get_json()
+        question = data['message']
+        limit = data['limit'] or 1 # this will result in 1
+        if limit>10: 
+            limit=10
 
-    language = 'he'
-    results = []
-    
-    for result in search(term=question, num_results=limit,advanced=True,lang=language):
-        results.append({"description":result.description,"title":result.title,"url":result.url})
+        language = 'he'
+        results = []
+        
+        for result in search(term=question, num_results=limit,advanced=True,lang=language):
+            results.append({"description":result.description,"title":result.title,"url":result.url})
 
-    return jsonify({'response': results})
+        return jsonify({'response': results})
+    except Exception as e:
+        return jsonify({'response': {'error':ERROR_MESSAGE,'data':repr(e)} })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
